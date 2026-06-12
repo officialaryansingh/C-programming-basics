4b64152d05c4a582d278170adfe707760f929bc5
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-12 12:40:57 +0530

    Add files via upload

---
+++ tut7.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int c = 45;
+int main(){
+    // int a,b,c;
+
+    // cout<<"Enter the value of a : ";
+    // cin>>a;
+    // cout<<endl;
+    // cout<<"Enter the value of b : ";
+    // cin>>b;
+    // cout<<endl;
+    // c = a+b;
+    // cout<<"the sum is : "<<c<<" -this is the local value of 'c'"<<endl;
+    // cout<<"the global value of c : "<<::c<<endl;//"::"- this is called scope resolution operator
+    //                                             // it calls the global value of c h
+
+
+
+    float d = 34.4;
+    long double e = 34.45468651;
+
+    cout<<"The value of d is : "<<d<<endl<<"The value of e is : "<<e;
+
+}
+
+
+
