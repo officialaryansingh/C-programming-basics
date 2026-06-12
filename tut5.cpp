8df708594cee60edccf47494b774d180375c0432
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-12 12:39:37 +0530

    Add files via upload

---
+++ tut5.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter the value of num1: ";/* '<<' - this is called Insertion Operater*/
+    cin>>num1; /*">>"- this is called Extraction operater*/
+
+    cout<<"Enter the value of num2: ";/* '<<' - this is called Insertion Operater*/
+    cin>>num2;/*">>"- this is called Extraction operater*/
+
+    cout<<"The sum is :"<< num1+num2;
+
+    return 0;
+}
