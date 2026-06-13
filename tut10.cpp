a919b2f190f684a02b7eebc3be482542f3eb42ee
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-13 22:07:09 +0530

    Add files via upload

---
+++ tut10.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+// struct employee
+// {
+//     /*  data   */
+//     int eId;
+//     char favChar;
+//     float salary;
+
+// };
+
+typedef struct employee
+{
+
+    /*  data  */
+    
+    int eId;
+    char favChar;
+    float salary;
+
+
+}ep;
+
+int main(){
+
+    struct employee NoOne;
+    NoOne.eId = 1;
+    NoOne.favChar = 'c';
+    NoOne.salary = 120000000;
+
+    cout<<"The value is "<<NoOne.eId<<endl;
+    cout<<"The value is "<<NoOne.favChar<<endl;
+    cout<<"The value is "<<int(NoOne.salary)<<endl;
+    return 0;
+
+}
