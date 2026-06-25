806c3e14a2ee19df3e72671e2c08127cc0c3e7e2
Aryansingh0089 <officialaryansingh6201890044@gmail.com>
2026-06-25 20:42:10 +0530

    Add files via upload
    created a program that take input of items of a class (in this file it's "shefs" ) with there respective assigned int values along with there price inputs 
    and displays there int the respective order of there iteration.

---
+++ 15_Object_memory_memory_allocation_array_in_classes.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class shop{
+    int itemId[100];
+    int itemPrice[100];
+    int counter;
+    
+    public:
+        void initCounter(void) { counter = 0;}
+        void setPrice(void);
+        void displayPrice(void);
+
+};
+
+void shop :: setPrice(void){
+    cout<<"Enter the itemId of the item no."<< (counter + 1) << endl;
+    cin>> itemId[counter];
+    cout<< "Enter the price of your item : " <<endl;
+    cin>> itemPrice[counter];
+
+    counter++;
+
+}
+
+void shop:: displayPrice(void){
+    for (int i = 0; i< counter ; i++){
+        cout<< "The price of item with Id : "<<itemId[i]<<" is "<<itemPrice[i]<<endl;
+    } 
+}
+
+int main(){
+    shop shefs;
+    shefs.initCounter();
+    for (int i = 0 ; i<= 4 ; i++){
+        shefs.setPrice();
+    }
+    shefs.displayPrice();
+
+    return 0;
+}
